3a80b2b4496a4796b4da295fda3179772a99d2ce
mtnori <moto44.noriyuki@gmail.com>
2023-12-09 17:38:01 +0900

    commit

--- cmd/invokelambda/main.go
+++ cmd/invokelambda/main.go
@@ -6,7 +6,7 @@ import (
 	"github.com/aws/aws-sdk-go-v2/aws"
 	awshttp "github.com/aws/aws-sdk-go-v2/aws/transport/http"
 	"github.com/aws/aws-sdk-go-v2/config"
-	"github.com/aws/aws-sdk-go-v2/credentials"
+	"github.com/aws/aws-sdk-go-v2/credentials/endpointcreds"
 	"github.com/aws/aws-sdk-go-v2/service/lambda"
 	"github.com/aws/aws-sdk-go-v2/service/lambda/types"
 	"log"
@@ -17,28 +17,49 @@ import (
 
 func main() {
 	wrapper := NewFunctionWrapper()
-	output := wrapper.Invoke("hello-world-sample", `{"key1":"aaaa"}`, true)
-	log.Println(string(output.Payload))
+	output1 := wrapper.Invoke("hello-world-sample", `{"key1":"aaaa"}`, true)
+	log.Println(string(output1.Payload))
+	output2 := wrapper.Invoke("hello-world-sample", `{"key1":"bbb"}`, true)
+	log.Println(string(output2.Payload))
+	output3 := wrapper.Invoke("hello-world-sample", `{"key1":"ccc"}`, true)
+	log.Println(string(output3.Payload))
+}
+
+type OptionFunc func(*endpointcreds.Options)
+
+func HTTPClient(client endpointcreds.HTTPClient) OptionFunc {
+	return func(args *endpointcreds.Options) {
+		args.HTTPClient = client
+	}
 }
 
 func NewFunctionWrapper() FunctionWrapper {
-	proxyCredentials := getCredentials()
-
+	//proxyCredentials := getCredentials()
+	//
 	customClient := awshttp.NewBuildableClient().WithTransportOptions(func(tr *http.Transport) {
-		proxyURL, err := url.Parse("PROXY URL")
+		proxyURL, err := url.Parse("http://PROXY_HOST:8080")
 		if err != nil {
 			log.Fatal(err)
 		}
 		tr.Proxy = http.ProxyURL(proxyURL)
 	})
+	//
+	//cfg, _ := config.LoadDefaultConfig(context.Background(),
+	//	config.WithCredentialsProvider(
+	//		credentials.NewStaticCredentialsProvider(
+	//			proxyCredentials.AccessKeyId,
+	//			proxyCredentials.SecretAccessKey,
+	//			proxyCredentials.Token)),
+	//	config.WithHTTPClient(customClient),
+	//)
 
 	cfg, _ := config.LoadDefaultConfig(context.Background(),
 		config.WithCredentialsProvider(
-			credentials.NewStaticCredentialsProvider(
-				proxyCredentials.AccessKeyId,
-				proxyCredentials.SecretAccessKey,
-				proxyCredentials.Token)),
-		config.WithHTTPClient(customClient),
+			endpointcreds.New(
+				"http://169.254.169.254/latest/meta-data/iam/security-credentials/ROLE_NAME",
+				HTTPClient(customClient),
+			),
+		),
 	)
 
 	return FunctionWrapper{
@@ -78,31 +99,31 @@ type ProxyCredentials struct {
 	Expiration      time.Time `json:"Expiration"`
 }
 
-func getCredentials() ProxyCredentials {
-	req, _ := http.NewRequest("GET", "http://169.254.169.254/latest/meta-data/iam/security-credentials/role-name", nil)
-
-	proxyUrl, _ := url.Parse("PROXY URL")
-	client := &http.Client{
-		Transport: &http.Transport{
-			Proxy: http.ProxyURL(proxyUrl),
-		},
-	}
-
-	resp, err := client.Do(req)
-	if err != nil {
-		log.Panicf("Couldn't access proxy server %v\n", err)
-	}
-	defer resp.Body.Close()
-
-	var proxyCredentials ProxyCredentials
-	if err := json.NewDecoder(resp.Body).Decode(&proxyCredentials); err != nil {
-		log.Panicf("Couldn't decode metadata response %v\n", err)
-	}
-
-	//log.Print(proxyCredentials.AccessKeyId)
-	//log.Print(proxyCredentials.SecretAccessKey)
-	//log.Print(proxyCredentials.Token)
-	log.Print(proxyCredentials.Expiration)
-
-	return proxyCredentials
-}
+//func getCredentials() ProxyCredentials {
+//	req, _ := http.NewRequest("GET", "http://169.254.169.254/latest/meta-data/iam/security-credentials/invoke-lambda-role", nil)
+//
+//	proxyUrl, _ := url.Parse("PROXY URL")
+//	client := &http.Client{
+//		Transport: &http.Transport{
+//			Proxy: http.ProxyURL(proxyUrl),
+//		},
+//	}
+//
+//	resp, err := client.Do(req)
+//	if err != nil {
+//		log.Panicf("Couldn't access proxy server %v\n", err)
+//	}
+//	defer resp.Body.Close()
+//
+//	var proxyCredentials ProxyCredentials
+//	if err := json.NewDecoder(resp.Body).Decode(&proxyCredentials); err != nil {
+//		log.Panicf("Couldn't decode metadata response %v\n", err)
+//	}
+//
+//	//log.Print(proxyCredentials.AccessKeyId)
+//	//log.Print(proxyCredentials.SecretAccessKey)
+//	//log.Print(proxyCredentials.Token)
+//	log.Print(proxyCredentials.Expiration)
+//
+//	return proxyCredentials
+//}
